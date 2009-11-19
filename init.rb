cd91914e08909057190fe2e4e6d642a94c3c1d51
Levente Bagi <bagilevi@gmail.com>
2009-11-19 15:07:44 +0100

    integrated editor: switch between form and markup

--- init.rb
+++ init.rb
@@ -1,10 +1,11 @@
 require 'relaxdb'
 require 'relaxedwiki/document'
+require 'relaxedwiki/historic_document'
+require 'relaxedwiki/changelog_controller'
 
 class RelaxDB::Document
-  def self.acts_as_wiki_document
+  def self.acts_as_relaxedwiki_document
     include RelaxedWiki::Document
   end
 end
-
 
--- lib/relaxedwiki/document.rb
+++ lib/relaxedwiki/document.rb
@@ -7,34 +7,37 @@ module RelaxedWiki
     def self.included(base)
       base.class_eval <<-EOS, __FILE__, __LINE__ + 1
         property :wiki_content
-          before_save(lambda do |doc|
-          if !doc.respond_to?(:wiki_content) || doc.wiki_content.nil? || doc.wiki_content.empty?
-            if doc.respond_to?(:generate_wiki_content) then
-              doc.generate_wiki_content
-            end
-          else
-            if doc.respond_to?(:parse_wiki_content) then
-              doc.parse_wiki_content
-            end
-          end
+        before_save(lambda do |doc|
+          doc.generate_or_parse_wiki_content
+          doc.create_historic_document
 	      end)
       EOS
     end
   
-    def update_wiki_content(params)
-
-      # 1. Create new history entry
-      require File.join(File.dirname(__FILE__), 'historic_document')
-      RelaxedWiki::HistoricDocument.create_from_edit(self, params[:change])
-
-      # 2. Update content in the document
-      self.wiki_content = params[:wiki_content]
-      if self.respond_to?(:parse_wiki_content) then
-        self.parse_wiki_content
+    def generate_or_parse_wiki_content
+      # if !self.respond_to?(:wiki_content) || self.wiki_content.nil? || self.wiki_content.empty?
+      if @wiki_change && @wiki_change[:type] == 'markup'
+        if self.respond_to?(:parse_wiki_content) then
+          # When user edited the markup
+          self.parse_wiki_content
+        end
+      else
+        if self.respond_to?(:generate_wiki_content) then
+          # When user edited by the form
+          self.generate_wiki_content
+        end
       end
-      self.save!
-
     end
+
+    # Called by RelaxDB::Document.set_attributes
+    def wiki_change=(params)
+      @wiki_change = params
+    end
+
+    def create_historic_document
+      RelaxedWiki::HistoricDocument.create_from_edit(self, @wiki_change)
+    end
+
   end
 
 end
